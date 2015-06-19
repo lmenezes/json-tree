4c2f74fa6fce5fcbe5a505027f92a1d4816bcbde
Leonardo Menezes <leonardo.menezes@xing.com>
2015-06-19 18:31:47 +0200

    added code style check

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,5 +51,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks("grunt-jscs");
   grunt.registerTask('dev', ['watch'])
-  grunt.registerTask('build', ['clean', 'copy', 'uglify']);
+  grunt.registerTask('build', ['jscs', 'clean', 'copy', 'uglify']);
 };
