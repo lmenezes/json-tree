e8c458de4239d2fb1bba22bbe0c10d2142dc949f
Leonardo Menezes <leonardo.menezes@xing.com>
2015-06-19 18:10:34 +0200

    added grunt to project

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      dist: {
+        src: ['dist/js']
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['src/*.js'],
+        tasks: ['build'],
+        options: {
+          spawn: false
+        }
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {
+            src: ['css/jsontree.css'],
+            dest: './dist/'
+          }
+        ]
+      }
+    },
+    jscs: {
+      src: ['src/jsontree.js'],
+      options: {
+        preset: 'google',
+        requireCamelCaseOrUpperCaseIdentifiers: "ignoreProperties"
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      my_target: {
+        files: {
+          'dist/js/jsontree.min.js': ['src/jsontree.js']
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks("grunt-jscs");
+  grunt.registerTask('dev', ['watch'])
+  grunt.registerTask('build', ['clean', 'copy', 'uglify']);
+};
